0c316e61dd1d488077a1c2898798cd38b24a4e86
Matt Delac <matt@delac.io>
2019-04-16 19:52:19 +0100

    Moved weather data to JavaScript

---
+++ 3/class-7/app/start/app.js
@@ -0,0 +1,13 @@
+let place = document.querySelector("#weather-city");
+let timestamp = document.querySelector("#weather-timestamp");
+let description = document.querySelector("#weather-description");
+let temperature = document.querySelector("#weather-temperature");
+let humidityLevel = document.querySelector("#weather-humidity-level");
+let windSpeed = document.querySelector("#weather-wind-speed");
+
+place.innerHTML = "New York";
+timestamp.innerHTML = "Tuesday 8:00 PM";
+description.innerHTML = "Sunny";
+temperature.innerHTML = 16;
+humidityLevel.innerHTML = 90;
+windSpeed.innerHTML = 2;
