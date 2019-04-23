398d3f57c4a96d23fe0f3a0646a2e2865757bbd4
Matt Delac <matt@delac.io>
2019-04-23 20:40:56 +0100

    Implemented feels like badges

--- 3/class-9/start/src/City.js
+++ 3/class-9/start/src/City.js
@@ -3,6 +3,18 @@ import CityTemperature from "./CityTemperature";
 import "./City.css";
 
 export default class City extends Component {
+  badge() {
+    if (this.props.temperature < 10) {
+      return <span className="badge badge-primary">cold</span>;
+    } else {
+      if (this.props.temperature < 20) {
+        return <span className="badge badge-warning">warm</span>;
+      } else {
+        return <span className="badge badge-danger">hot</span>;
+      }
+    }
+  }
+
   render() {
     return (
       <tr>
@@ -17,9 +29,7 @@ export default class City extends Component {
             unit="imperial"
           />
         </td>
-        <td>
-          <span className="badge badge-danger">Hot</span>
-        </td>
+        <td>{this.badge()}</td>
         <td>
           <button type="button" className="btn btn-outline-light btn-sm">
             Learn more
