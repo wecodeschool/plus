045b9f7f2981e610cd8cee47ae232b5946da2a38
Matt Delac <matt@delac.io>
2019-06-25 21:01:23 +0100

    Add the search functionnility

--- 4/class-6/app/start/script.js
+++ 4/class-6/app/start/script.js
@@ -19,6 +19,18 @@ function formatDate(date) {
   return `${days[day]} ${date.getHours()}:${minutes}`;
 }
 
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let keyword = document.querySelector("#keyword");
+  search(keyword.value);
+}
+
 function showWeather(response) {
   let temperature = document.querySelector("#temperature");
   temperature.innerText = Math.round(response.data.main.temp);
@@ -45,7 +57,6 @@ function showWeather(response) {
   );
 }
 
-let city = "Lisbon";
-let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(showWeather);
+let form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
+search("Lisbon,pt");
