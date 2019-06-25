3537a30f153db31bce0d32d75de1e89a44da9181
Matt Delac <matt@delac.io>
2019-06-25 20:41:39 +0100

    Rename function name

--- 4/class-6/app/start/script.js
+++ 4/class-6/app/start/script.js
@@ -19,7 +19,7 @@ function formatDate(date) {
   return `${days[day]} ${date.getHours()}:${minutes}`;
 }
 
-function showTemperature(response) {
+function showWeather(response) {
   let temperature = document.querySelector("#temperature");
   temperature.innerText = Math.round(response.data.main.temp);
 
@@ -43,4 +43,4 @@ function showTemperature(response) {
 let city = "Lisbon";
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(showTemperature);
+axios.get(apiUrl).then(showWeather);
