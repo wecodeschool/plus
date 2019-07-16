f96633821648f82a9c669a18fff7785a4af6dcd7
Matt Delac <matt@delac.io>
2019-07-16 20:48:46 +0100

    Add comment to explain how timezone works

--- 4/week-6/start/src/HumanDate.js
+++ 4/week-6/start/src/HumanDate.js
@@ -3,6 +3,9 @@ import React, { Component } from "react";
 export default class HumanDate extends Component {
   humanizeDate = () => {
     // Had to multiply the timestamp by 1000 to make it compatible
+    // Add timezone difference to the date
+    // Substract 1 hour (3600 seconds) because of summer time
+    // Talk to Maria if problem
     let date = new Date(
       this.props.timestamp * 1000 + (this.props.timezone - 3600) * 1000
     );
