9d9f2d2a1fb89fcde9e9cd3064d3307d8e5ca3e7
Matt Delac <matt@delac.io>
2019-07-16 19:04:06 +0100

    Styled the content

--- 4/week-6/start/src/App.js
+++ 4/week-6/start/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import Weather from "./Weather";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Weather city="Lisbon" />
+      <div className="container">
+        <div className="card m-5">
+          <div className="card-body">
+            <Weather city="Lisbon" />
+          </div>
+        </div>
       </div>
     );
   }
--- 4/week-6/start/src/Weather.js
+++ 4/week-6/start/src/Weather.js
@@ -1,22 +1,36 @@
 import React, { Component } from "react";
+import "./Weather.css";
 
 export default class Weather extends Component {
   render() {
     return (
       <div>
-        <h1>Lisbon</h1>
+        <h1>{this.props.city}</h1>
         <ul>
           <li>Tuesday 18:00</li>
           <li>Partly Cloudy</li>
         </ul>
-        <div className="temperature">
-          21<small>°C</small>
+        <div className="row">
+          <div className="col-sm-6 ">
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt=""
+                className="float-left"
+              />
+              <div className="temperature float-left">
+                21<small>°C</small>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <ul>
+              <li>Precipitation: 0%</li>
+              <li>Humidity: 74%</li>
+              <li>Wind: 26 km/h</li>
+            </ul>
+          </div>
         </div>
-        <ul>
-          <li>Precipitation: 0%</li>
-          <li>Humidity: 74%</li>
-          <li>Wind: 26 km/h</li>
-        </ul>
       </div>
     );
   }
